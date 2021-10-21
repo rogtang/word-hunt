ec1fbf2e4ba99d63b140ee3e6fc0c4affaeca24f
rogtang <rogtang@gmail.com>
2021-10-20 18:52:03 -0700

    pwa upload

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -10,3 +10,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+serviceWorkerRegistration.register();
